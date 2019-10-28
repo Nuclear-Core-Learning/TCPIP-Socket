58c39a97a8d648e8aa205d2a56c31dd13b69be93
boonyachengdu <boonyachengdu@gmail.com>
2019-10-28 23:33:20 +0800

    文件压缩

--- src/Chapter4/CompressClient.java
+++ src/Chapter4/CompressClient.java
@@ -1,4 +1,5 @@
 package Chapter4;
+
 import java.net.Socket;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,52 +13,51 @@ import java.io.FileOutputStream;
 
 public class CompressClient {
 
-  public static final int BUFSIZE = 256;  // Size of read buffer
+	public static final int BUFSIZE = 256; // Size of read buffer
 
-  public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-    if (args.length != 3) { // Test for correct # of args
-      throw new IllegalArgumentException("Parameter(s): <Server> <Port> <File>");
-    }
+		if (args.length != 3) { // Test for correct # of args
+			throw new IllegalArgumentException("Parameter(s): <Server> <Port> <File>");
+		}
 
-    String server = args[0];               // Server name or IP address
-    int port = Integer.parseInt(args[1]);  // Server port
-    String filename = args[2];             // File to read data from
+		String server = args[0]; // Server name or IP address
+		int port = Integer.parseInt(args[1]); // Server port
+		String filename = args[2]; // File to read data from
 
-    // Open input and output file (named input.gz)
-    FileInputStream fileIn = new FileInputStream(filename);
-    FileOutputStream fileOut = new FileOutputStream(filename + ".gz");
-  
-    // Create socket connected to server on specified port
-    Socket sock = new Socket(server, port);
+		// Open input and output file (named input.gz)
+		FileInputStream fileIn = new FileInputStream(filename);
+		FileOutputStream fileOut = new FileOutputStream(filename + ".gz");
 
-    // Send uncompressed byte stream to server
-    sendBytes(sock, fileIn);
+		// Create socket connected to server on specified port
+		Socket sock = new Socket(server, port);
 
-    // Receive compressed byte stream from server
-    InputStream sockIn = sock.getInputStream();
-    int bytesRead;                      // Number of bytes read
-    byte[] buffer = new byte[BUFSIZE];  // Byte buffer
-    while ((bytesRead = sockIn.read(buffer)) != -1) {
-      fileOut.write(buffer, 0, bytesRead);
-      System.out.print("R");   // Reading progress indicator
-    }
-    System.out.println();      // End progress indicator line
+		// Send uncompressed byte stream to server
+		sendBytes(sock, fileIn);
 
-    sock.close();     // Close the socket and its streams
-    fileIn.close();   // Close file streams
-    fileOut.close();
-  }
+		// Receive compressed byte stream from server
+		InputStream sockIn = sock.getInputStream();
+		int bytesRead; // Number of bytes read
+		byte[] buffer = new byte[BUFSIZE]; // Byte buffer
+		while ((bytesRead = sockIn.read(buffer)) != -1) {
+			fileOut.write(buffer, 0, bytesRead);
+			System.out.print("R"); // Reading progress indicator
+		}
+		System.out.println(); // End progress indicator line
 
-  private static void sendBytes(Socket sock, InputStream fileIn) 
-      throws IOException {
-    OutputStream sockOut = sock.getOutputStream();
-    int bytesRead;                      // Number of bytes read
-    byte[] buffer = new byte[BUFSIZE];  // Byte buffer
-    while ((bytesRead = fileIn.read(buffer)) != -1) {
-      sockOut.write(buffer, 0, bytesRead);
-      System.out.print("W");   // Writing progress indicator
-    }
-    sock.shutdownOutput();     // Finished sending
-  }
+		sock.close(); // Close the socket and its streams
+		fileIn.close(); // Close file streams
+		fileOut.close();
+	}
+
+	private static void sendBytes(Socket sock, InputStream fileIn) throws IOException {
+		OutputStream sockOut = sock.getOutputStream();
+		int bytesRead; // Number of bytes read
+		byte[] buffer = new byte[BUFSIZE]; // Byte buffer
+		while ((bytesRead = fileIn.read(buffer)) != -1) {
+			sockOut.write(buffer, 0, bytesRead);
+			System.out.print("W"); // Writing progress indicator
+		}
+		sock.shutdownOutput(); // Finished sending
+	}
 }
