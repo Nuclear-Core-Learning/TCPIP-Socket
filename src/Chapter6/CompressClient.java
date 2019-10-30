b28ec9f0962b22a7a0aac53cf8993031d3cf6df1
boonyachengdu <boonyachengdu@gmail.com>
2019-10-30 23:13:30 +0800

    Socket死锁

--- src/Chapter6/CompressClient.java
+++ src/Chapter6/CompressClient.java
@@ -1,4 +1,5 @@
 package Chapter6;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -8,59 +9,66 @@ import java.net.Socket;
 
 public class CompressClient {
 
-  public static final int BUFSIZE = 256;  // Size of read buffer
+	public static final int BUFSIZE = 256; // Size of read buffer
 
-  public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
+		
+		if(args.length==0) {
+			args=new String[3];
+			args[0]="127.0.0.1";
+			args[1]="5433";
+			args[2]="C:\\Users\\Administrator\\Desktop\\test.txt";
+		}
 
-    if (args.length != 3)  // Test for correct #  of args
-      throw new IllegalArgumentException("Parameter(s): <Server> <Port> <File>");
+		if (args.length != 3) // Test for correct # of args
+			throw new IllegalArgumentException("Parameter(s): <Server> <Port> <File>");
 
-    String server = args[0];               // Server name or IP address
-    int port = Integer.parseInt(args[1]);  // Server port
-    String filename = args[2];             // File to read data from
+		String server = args[0]; // Server name or IP address
+		int port = Integer.parseInt(args[1]); // Server port
+		String filename = args[2]; // File to read data from
 
-    // Open input and output file (named input.gz)
-    final FileInputStream fileIn = new FileInputStream(filename);
-    FileOutputStream fileOut = new FileOutputStream(filename + ".gz");
-  
-    // Create socket connected to server on specified port
-    final Socket sock = new Socket(server, port);
+		// Open input and output file (named input.gz)
+		final FileInputStream fileIn = new FileInputStream(filename);
+		FileOutputStream fileOut = new FileOutputStream(filename + ".gz");
 
-    // Send uncompressed byte stream to server
-    Thread thread = new Thread() {
-      public void run() {
-        try {
-          SendBytes(sock, fileIn);
-        } catch (Exception ignored) {}
-      }
-    };
-    thread.start();
+		// Create socket connected to server on specified port
+		final Socket sock = new Socket(server, port);
 
-    // Receive compressed byte stream from server
-    InputStream sockIn = sock.getInputStream();
-    int bytesRead;                      // Number of bytes read
-    byte[] buffer = new byte[BUFSIZE];  // Byte buffer
-    while ((bytesRead = sockIn.read(buffer)) != -1) {
-      fileOut.write(buffer, 0, bytesRead);
-      System.out.print("R");   // Reading progress indicator
-    }
-    System.out.println();      // End progress indicator line
+		// Send uncompressed byte stream to server
+		Thread thread = new Thread() {
+			public void run() {
+				try {
+					SendBytes(sock, fileIn);
+				} catch (Exception ignored) {
+				}
+			}
+		};
+		thread.start();
 
-    sock.close();     // Close the socket and its streams
-    fileIn.close();   // Close file streams
-    fileOut.close();
-  }
+		// Receive compressed byte stream from server
+		InputStream sockIn = sock.getInputStream();
+		int bytesRead; // Number of bytes read
+		byte[] buffer = new byte[BUFSIZE]; // Byte buffer
+		while ((bytesRead = sockIn.read(buffer)) != -1) {
+			fileOut.write(buffer, 0, bytesRead);
+			System.out.print("R"); // Reading progress indicator
+		}
+		System.out.println(); // End progress indicator line
 
-  public static void SendBytes(Socket sock, InputStream fileIn)
-      throws IOException {
+		sock.close(); // Close the socket and its streams
+		fileIn.close(); // Close file streams
+		fileOut.close();
+	}
 
-    OutputStream sockOut = sock.getOutputStream();
-    int bytesRead;                      // Number of bytes read
-    byte[] buffer = new byte[BUFSIZE];  // Byte buffer
-    while ((bytesRead = fileIn.read(buffer)) != -1) {
-      sockOut.write(buffer, 0, bytesRead);
-      System.out.print("W");   // Writing progress indicator
-    }
-    sock.shutdownOutput();     // Done sending
-  }
+	public static void SendBytes(Socket sock, InputStream fileIn) throws IOException {
+
+		OutputStream sockOut = sock.getOutputStream();
+		int bytesRead; // Number of bytes read
+		byte[] buffer = new byte[BUFSIZE]; // Byte buffer
+		while ((bytesRead = fileIn.read(buffer)) != -1) {
+			sockOut.write(buffer, 0, bytesRead);
+			System.out.print("W"); // Writing progress indicator
+		}
+		sock.shutdownOutput(); // Done sending
+	}
 }
